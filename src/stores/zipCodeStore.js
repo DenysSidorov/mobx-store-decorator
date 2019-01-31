e5d947183a6e5114ed212afef11ab5918ec462ff
Denys Sidorov <000scorpions000@gmail.com>
2019-02-01 01:12:11 +0200

    normalise store

--- src/stores/zipCodeStore.js
+++ src/stores/zipCodeStore.js
@@ -11,54 +11,14 @@ class ZipCodeStore {
 
 
   constructor(cities = [], initialState = {}) {
-
     this.zipCodeItems = observable(cities);
-
     extendObservable(this, initialState);
   }
 
-  @action('set current  zip code')
-  setCurrentItem = (object) => {
-    this.currentItem = object;
-  }
-
-  @action('set search value')
-  setSearchValue = (string) => {
-    this.searchValue = string;
-  }
-
-  @action('set error value')
-  setErrorValue = (string) => {
-    this.searchError = string;
-  }
-
-  @computed
-  get getCurrentItem () {
-    return this.currentItem;
-  }
-
-  @computed
-  get getSearchValue () {
-    return this.searchValue;
-  }
-
-  @computed
-  get getSearchError () {
-    return this.searchError;
-  }
-
-  @computed
-  get getFetchingState() {
-    return this.isFetching;
-  }
+  /** ZIP codes - START*/
   @computed
   get getZipCodeItems() {
     return this.zipCodeItems;
-  }
-
-  @action('set fetching state')
-  setFetchingState = (boolean) => {
-    this.isFetching = boolean;
   }
 
   @action('set zip code items ')
@@ -73,6 +33,62 @@ class ZipCodeStore {
     this.zipCodeItems.push(zipCodeObject);
   }
 
+  /** ZIP codes - END*/
+
+
+  /** Fetching state - START*/
+  @computed
+  get getFetchingState() {
+    return this.isFetching;
+  }
+
+  @action('set fetching state')
+  setFetchingState = (boolean) => {
+    this.isFetching = boolean;
+  }
+
+  /** Fetching state  - END*/
+
+
+  /** Current ZIP code - START*/
+  @computed
+  get getCurrentItem() {
+    return this.currentItem;
+  }
+
+  @action('set current zip code')
+  setCurrentItem = (object) => {
+    this.currentItem = object;
+  }
+
+  /** Current ZIP code  - END*/
+
+
+  /** Search value - START*/
+  @computed
+  get getSearchValue() {
+    return this.searchValue;
+  }
+
+  @action('set search value')
+  setSearchValue = (string) => {
+    this.searchValue = string;
+  }
+
+  /** Search value  - END*/
+
+
+  /** Search error - START*/
+  @computed
+  get getSearchError() {
+    return this.searchError;
+  }
+
+  @action('set error value')
+  setErrorValue = (string) => {
+    this.searchError = string;
+  }
+  /** Search error  - END*/
 }
 
 const zipCodeStore = new ZipCodeStore(cities);
